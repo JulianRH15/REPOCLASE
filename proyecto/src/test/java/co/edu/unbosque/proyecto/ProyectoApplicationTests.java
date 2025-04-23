b38fc3cb5a28f373d88dff65f8551bf2f5d47141
JulianRH15 <julianrh734@gmail.com>
2025-04-22 20:56:40 -0500

    Arreglo 9
    si

--- proyecto/src/test/java/co/edu/unbosque/proyecto/ProyectoApplicationTests.java
+++ proyecto/src/test/java/co/edu/unbosque/proyecto/ProyectoApplicationTests.java
@@ -1,14 +1,14 @@
-package co.edu.unbosque.proyecto;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProyectoApplicationTests {
-
-	@Test
-	void contextLoads() {
-		System.out.println("ola");
-	}
-
-}
+//package co.edu.unbosque.proyecto;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProyectoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//		System.out.println("ola");
+//	}
+//
+//}
